5f7d7ce96286075c2331a709cd2cd8c61e7ab6fd
Andre Silva <andreswebs@pm.me>
2022-01-25 14:11:31 -0300

    fix param ARN

--- main.tf
+++ main.tf
@@ -4,8 +4,8 @@ data "aws_partition" "current" {}
 
 locals {
   param_arn_prefix = "arn:${data.aws_partition.current.partition}:ssm:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:parameter"
-  param_arns       = [for p in var.parameter_names : "${local.param_arn_prefix}:${p}"]
-  param_path_arns  = [for p in var.parameter_names : "${local.param_arn_prefix}:${p}/*"]
+  param_arns       = [for p in var.parameter_names : "${local.param_arn_prefix}${p}"]
+  param_path_arns  = [for p in var.parameter_names : "${local.param_arn_prefix}${p}/*"]
   param_arns_all   = concat(local.param_arns, local.param_path_arns)
 }
 
